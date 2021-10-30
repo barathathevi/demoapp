9674acc6c35e52b2df6b870cdf5989c0cb28c999
Barathathevi <unique.barathi@gmail.com>
2021-10-30 20:07:01 +0530

    Initial commit

---
+++ src/test/java/com/simplilearn/demoapp/DemoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.demoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
